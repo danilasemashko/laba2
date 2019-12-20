3b4c4e19529b2c9092f8d706bfa9dd5f95d28944
danilasemashko <danila.semashko.1994@mail.ru>
2019-12-20 15:21:20 +0300

    laba2

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Formula frame = new Formula();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
